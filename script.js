18a179b540e7d30ea7f0d2f75f3e4831e02a9cd9
ioanagavreliuc <127242866+ioanagavreliuc@users.noreply.github.com>
2023-05-22 22:01:16 +0300

    hamburger menu script<3 (momentan nu prea merge)

---
+++ script.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  document.getElementById("navLinks").style.right="0";
+}
+
+function hideMenu() {
+  document.getElementById("navLinks").style.right="-200px";
+}
